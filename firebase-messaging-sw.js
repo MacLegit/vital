83f96efad694c6e1cec9bac813935ed308d8964f
Mac Ronald <rw7.ronaldwilliam@gmail.com>
2025-09-18 17:56:35 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,10 +1,12 @@
-importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyCdvG19MnplrrKlfZdsx6VFIsdLkTHbP3o",
   authDomain: "vital-e22d5.firebaseapp.com",
+  databaseURL: "https://vital-e22d5-default-rtdb.firebaseio.com",
   projectId: "vital-e22d5",
+  storageBucket: "vital-e22d5.appspot.com",
   messagingSenderId: "76460889955",
   appId: "1:76460889955:web:8e82a93e29357d1a6ce556"
 });
@@ -12,6 +14,9 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(payload => {
-  const { title, body } = payload.notification;
-  self.registration.showNotification(title, { body });
+  console.log("Mensagem em segundo plano:", payload);
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon.png" // opcional, adicione um ícone no repo
+  });
 });
